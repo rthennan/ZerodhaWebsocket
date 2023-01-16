63825e1268817db2080c7dfe88cf791692fcc24b
Rajesh Thennan <38931343+rthennan@users.noreply.github.com>
2023-01-16 20:43:57 +0530

    Added Retries for access token requests

--- DAS5/accessTokenReqDAS6.py
+++ DAS5/accessTokenReqDAS6.py
@@ -52,99 +52,108 @@ def log(txt):
     logMsg = '\n'+str(datetime.datetime.now())+'    ' + str(txt)
     with open(logFile,'a') as f:
         f.write(logMsg)
-
-try:
-    otpchangers = [29,30,31,59,0,1]
-    conn = MySQLdb.connect(host = mysqlHost, user = mysqlUser, passwd = mysqlPass)
-    conn.autocommit(True)
-    c = conn.cursor()
-    c.execute("create DATABASE if not exists aws_tokens")
-    c.execute("create table if not exists aws_tokens.das6 (time DATETIME UNIQUE,requestUrl varchar(255) ,reqToken varchar(255) ,accessToken varchar(255))")
-    shortsql = "INSERT into aws_tokens.das6 values (%s, %s, %s, %s)"
-    """Using DAS6 App"""
-    apikey = apiKey2
-    apisec = apisec2
-    """Using DAS App"""
-    kite = KiteConnect(api_key = apikey)
-    url = (kite.login_url())
-    loginName = 'LG6209'
-    password = ZerodhaPass
-
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
-
-    log("DAS6 - Access Token - Execution started")
-    print("DAS6 - Access Token - Execution started")
-
-    driver.get(url)
-    log("DAS6 - Entered Try Block")
-    "DAS6 - Access Token - Entered Try Block"
-
-    #Wait for the Page to Load completely
-    print("Opening the main Login Page")
-    time.sleep(10)
-    #Enter credentials and submit
-    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[1]/input').send_keys(loginName)
-    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[2]/input').send_keys(password)
-    driver.find_element_by_xpath('//*[@id="container"]/div/div/div/form/div[4]/button').click()
-    print("Credentials Entered and submitted. Waiting for 2FA to load")
-    #Wait for the 2FA Page to Load completely
-    time.sleep(10)
-    #Enter OTP and click submit    
-    if usingTOTP:
-        totp = pyotp.TOTP(TOTP_pin)
-        otpchangers = [29,30,31,59,0,1]
-            #Waiting if OTP is about to change
-        while datetime.datetime.now().second in otpchangers:
-            log('otpchanger '+str(datetime.datetime.now().second))
-            time.sleep(1)
-            #Enter TOTP
-        driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[2]/input').send_keys(totp.now())
-    else:
-        #Enter Pin
-        driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[2]/input').send_keys(TOTP_pin) #Changed on 2022-09-26 
-    #Submit
-    driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[3]/button').click() #Changed on 2022-09-25
-    #Submit
-    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[3]/button').click()
-    #Wait for the 2FA Page to Load completely
-    print("DAS6 - Pin entered and submitted.")
-    time.sleep(10)
-    #Capture the redirect URL
-    tokenUrl = (driver.current_url)
-    reqToken = tokenUrl
-    #Token fetch code start
+numbOfRetries = 5
+for attempt in range(1,numbOfRetries+1):
 	
-    lenReq = len("request_token=")
-    reqToken = reqToken[reqToken.find('request_token=')+lenReq:]
-    endChar = reqToken.find('&')
-    if endChar!=-1:
-        reqToken = reqToken[:endChar]
-    #Token fetch code end
-    url = "Response URL: " +tokenUrl
-    tok = "Request Token: " +reqToken
-    print(url)
-    print(tok)
-    log(url)
-    log(tok)
-    print("Retrieving Access Token")
-    session = kite.generate_session(reqToken, apisec)
-    accessToken = session['access_token']
-    msg = "Access Token: "+accessToken
-    print(msg)
-    log(msg)
-    c.execute(shortsql,[datetime.datetime.now(),tokenUrl,reqToken,accessToken])
-    c.close()
-    conn.close()
-    log('Updated in DB')
-    print('Updated in DB')
-    msg = 'DAS6: Access Token Successful'
-    log(msg)
-    print(msg)
-    mail(destinationEmailAddress,msg,'DAS6: Access Token succeeded at '+str(datetime.datetime.now())+'\n Proceeding to Instrument List file lookup')
+	try:
+	    otpchangers = [29,30,31,59,0,1]
+	    conn = MySQLdb.connect(host = mysqlHost, user = mysqlUser, passwd = mysqlPass)
+	    conn.autocommit(True)
+	    c = conn.cursor()
+	    c.execute("create DATABASE if not exists aws_tokens")
+	    c.execute("create table if not exists aws_tokens.das6 (time DATETIME UNIQUE,requestUrl varchar(255) ,reqToken varchar(255) ,accessToken varchar(255))")
+	    shortsql = "INSERT into aws_tokens.das6 values (%s, %s, %s, %s)"
+	    """Using DAS6 App"""
+	    apikey = apiKey2
+	    apisec = apisec2
+	    """Using DAS App"""
+	    kite = KiteConnect(api_key = apikey)
+	    url = (kite.login_url())
+	    loginName = 'LG6209'
+	    password = ZerodhaPass
 
-except Exception as e:
-    log(str(e))
-    print("DAS6 - Access token Failed :" + str(e))
-    mail(destinationEmailAddress, 'DAS7 - Access Token Failed',str(e))
+	    options = Options()
+	    options.headless = True
+	    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+
+	    log("DAS6 - Access Token - Execution started")
+	    print("DAS6 - Access Token - Execution started")
+
+	    driver.get(url)
+	    log("DAS6 - Entered Try Block")
+	    "DAS6 - Access Token - Entered Try Block"
+
+	    #Wait for the Page to Load completely
+	    print("Opening the main Login Page")
+	    time.sleep(10)
+	    #Enter credentials and submit
+	    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[1]/input').send_keys(loginName)
+	    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[2]/input').send_keys(password)
+	    driver.find_element_by_xpath('//*[@id="container"]/div/div/div/form/div[4]/button').click()
+	    print("Credentials Entered and submitted. Waiting for 2FA to load")
+	    #Wait for the 2FA Page to Load completely
+	    time.sleep(10)
+	    #Enter OTP and click submit    
+	    if usingTOTP:
+		totp = pyotp.TOTP(TOTP_pin)
+		otpchangers = [29,30,31,59,0,1]
+		    #Waiting if OTP is about to change
+		while datetime.datetime.now().second in otpchangers:
+		    log('otpchanger '+str(datetime.datetime.now().second))
+		    time.sleep(1)
+		    #Enter TOTP
+		driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[2]/input').send_keys(totp.now())
+	    else:
+		#Enter Pin
+		driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[2]/input').send_keys(TOTP_pin) #Changed on 2022-09-26 
+	    #Submit
+	    driver.find_element_by_xpath('//*[@id="container"]/div[1]/div/div/form/div[3]/button').click() #Changed on 2022-09-25
+	    #Submit
+	    driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/form/div[3]/button').click()
+	    #Wait for the 2FA Page to Load completely
+	    print("DAS6 - Pin entered and submitted.")
+	    time.sleep(10)
+	    #Capture the redirect URL
+	    tokenUrl = (driver.current_url)
+	    reqToken = tokenUrl
+	    #Token fetch code start
+
+	    lenReq = len("request_token=")
+	    reqToken = reqToken[reqToken.find('request_token=')+lenReq:]
+	    endChar = reqToken.find('&')
+	    if endChar!=-1:
+		reqToken = reqToken[:endChar]
+	    #Token fetch code end
+	    url = "Response URL: " +tokenUrl
+	    tok = "Request Token: " +reqToken
+	    print(url)
+	    print(tok)
+	    log(url)
+	    log(tok)
+	    print("Retrieving Access Token")
+	    session = kite.generate_session(reqToken, apisec)
+	    accessToken = session['access_token']
+	    msg = "Access Token: "+accessToken
+	    print(msg)
+	    log(msg)
+	    c.execute(shortsql,[datetime.datetime.now(),tokenUrl,reqToken,accessToken])
+	    c.close()
+	    conn.close()
+	    log('Updated in DB')
+	    print('Updated in DB')
+	    msg = 'DAS6: Access Token Successful'
+	    log(msg)
+	    print(msg)
+	    mail(destinationEmailAddress,msg,'DAS6: Access Token succeeded at '+str(datetime.datetime.now())+'\n Proceeding to Instrument List file lookup')
+
+	except Exception as e:
+		msg = 'DAS7 - Access token Failed. Attempt No :'+str(attempt)+' Exception-> ' + str(e)
+		log(msg)
+		print(msg)
+
+	else:
+        	break
+else:
+    msg = 'DAS6 - Access token Failed After '+str(numbOfRetries)+' attempts.'
+    log(msg)
+    mail(destinationEmailAddress, 'DAS5 - Access token Failed.',msg)  	
