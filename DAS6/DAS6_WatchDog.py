67c25526d1d26ddd13d4e341fb959cd842a9ffdd
Rajesh Thennan <38931343+rthennan@users.noreply.github.com>
2023-02-20 11:40:25 +0530

    Update DAS6_WatchDog.py

--- DAS6/DAS6_WatchDog.py
+++ DAS6/DAS6_WatchDog.py
@@ -14,6 +14,9 @@ destinationEmailAddress = creds['destinationEmailAddress']
 marketCloseHH = 15
 marketCloseMM = 30
 
+def printer(inText):
+    print(f'{str(datetime.now())[:19]} {inText}')
+
 def log(txt):
 	directory = path.join('Logs',str(date.today())+'_DAS6Logs')
 	if not path.exists(directory):
@@ -42,6 +45,7 @@ def lastTimeCheck(fileName):
 
 
 log('DAS6 WatchDog started')
+printer('DAS6 WatchDog started')
 
 while (datetime.now().replace(second=0, microsecond=0) < datetime.now().replace(hour=marketCloseHH, minute=marketCloseMM,second=0, microsecond=0)):
     nfoLastEntry = lastTimeCheck('_DAS6_NFO_FULL.log')
@@ -58,11 +62,15 @@ while (datetime.now().replace(second=0, microsecond=0) < datetime.now().replace(
         if bnfoLastEntry>360:
             msg = msg + f'BNFO stalled. It has been {round(bnfoLastEntry/60)} minutes since the last connection message\n'   
         log(f"STALLED: {msg}")
+        printer(f"STALLED: {msg}")
         mail(destinationEmailAddress,f"DAS6 WatchDog {date.today()}:  DAS6 STALLED",msg)
     else:
-        log('NFO and BNFO seem to be alive')
+        msg = f'NFO and BNFO seem to be alive.\nTime since last entry in DAS6_NFO_FULL: {nfoLastEntry}.\nTime since last entry in DAS6_BNFO_FULL: {bnfoLastEntry}'
+        log(msg)
+        printer(msg)
     
     #Sleeping for 5 minutes
     sleep(300)
 
 log('DAS6 WatchDog - Done for the day')
+printer('DAS6 WatchDog - Done for the day')
